58d4dd014bea60c422268210badaa717cfb6d5ce
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-27 12:34:44 +0530

    test: update widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,7 @@ import 'package:puzzle/main.dart';
 void main() {
   testWidgets('Puzzle game mounts successfully', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const GainPuzzleApp());
+    await tester.pumpWidget(const TopPuzzleApp());
 
     // Verify that the game screen loads by checking for HUD text
     expect(find.textContaining('TIME:'), findsOneWidget);
