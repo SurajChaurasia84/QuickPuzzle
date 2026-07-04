5fee4da57f12c25d0d3f951e0243b38eca03512f
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-07-04 23:01:52 +0530

    chore(android): update Android configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.quick.puzzleapp"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
